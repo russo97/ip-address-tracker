f4c64fde5a3c9483a9cd5e62ec5f6e6e28a57f96
Wanderson Silva <wandersonsilva768@gmail.com>
2021-04-03 10:14:56 -0300

    feat: configuração inicial do componente Map

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="app">
     <Header />
+
+    <Map />
   </div>
 </template>
 
 <script>
+  import Map from "./components/Map.vue";
   import Header from "./components/Header.vue";
 
   export default {
     name: "App",
 
     components: {
+      Map,
       Header
     }
   }
 </script>
 
 <style lang="scss">
+  #app {
+    display: flex;
+    flex-direction: column;
+  }
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mapview">
+    <div id="mapView"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Map"
+  }
+</script>
+
+<style lang="scss">
+  .mapview {
+    flex: 1 100%;
+    background-color: red;
+  }
+</style>
