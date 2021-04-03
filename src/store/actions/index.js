65aa578a540b76163953ebc33373fa31a8088584
Wanderson Silva <wandersonsilva768@gmail.com>
2021-04-03 08:38:40 -0300

    feat: buscando URL atraves das variáveis de ambiente

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,8 @@
 
 const captureUserIP = async ({ commit }) => {
-  const ipinfo = await ( await fetch('http://www.geoplugin.net/json.gp') ).json();
+  const URL_FETCH = import.meta.env.VITE_GEO_PLUGIN;
+
+  const ipinfo = await ( await fetch(URL_FETCH) ).json();
 
   commit('CAPTURE_USER_IP', await ipinfo.geoplugin_request);
 };
