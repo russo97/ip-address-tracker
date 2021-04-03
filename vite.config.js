61b9632071d86db2816c16ec90f3463a096d1917
Wanderson Silva <wandersonsilva768@gmail.com>
2021-04-03 08:27:51 -0300

    feat: configuração de variável de ambiente contendo as chaves das APIs

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,27 @@ import vue from '@vitejs/plugin-vue'
 const path = require('path');
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@sass': path.resolve(__dirname, 'src', 'assets', 'sass'),
-      '@images': path.resolve(__dirname, 'src', 'assets', 'images')
-    }
-  },
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "@sass/variables";
-          @import "@sass/mixins";
-        `
-      },
+export default ({  }) => {
+  return defineConfig({
+    plugins: [vue()],
+  
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@sass': path.resolve(__dirname, 'src', 'assets', 'sass'),
+        '@images': path.resolve(__dirname, 'src', 'assets', 'images')
+      }
     },
-  },
-})
+  
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "@sass/variables";
+            @import "@sass/mixins";
+          `
+        },
+      },
+    }
+  })
+}
